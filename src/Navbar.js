085190184cfaf5424c04ed438157b8ec6355f5da
Nate Hamshar <williamhamshar@gmail.com>
2022-06-19 18:04:40 -0400

    tried to do some animations when rendering, - prob will delete and make new ones

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <div className="Navbar">
             <ul className="nav-list">
-                <li><a href="#welcome" className="nav-links">Home</a></li>
+                <li><a href="#welcome" className="nav-links">Top</a></li>
                 <li><a href="#about" className="nav-links">About</a></li>
                 <li><a href="#skills" className="nav-links">Skills</a></li>
                 <li><a href="#projects" className="nav-links">Projects</a></li>

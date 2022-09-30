ac11d87220896e24752be5fadcaf4421e2228712
Nate Hamshar <williamhamshar@gmail.com>
2022-09-30 19:55:50 -0400

    added media Qs and the money project

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ function App() {
           <ul className='skills-list'>
             <li>HTML5, CSS3, and JS</li>
             <li>Experience using D3, SASS, Boostrap, Semantic UI, and other libraries</li>
-            <li>Most comfortable using the React, but always looking to expand my knowledge</li>
-            <li>Using APIs, with Axios</li>
+            <li>Most comfortable using React, but always looking to expand my knowledge</li>
+            <li>Comfortable working with APIs, and using Axios</li>
             <li>Version control with Git - <a rel="noreferrer" target="_blank" href='https://github.com/nateHamshar' className='gitLink'>My GitHub</a></li>
           </ul>
         </div>
@@ -44,15 +44,15 @@ function App() {
         <h1 className="projects-header">Projects I've Worked On</h1>
         <div className='project-section'>
         <div className="project-container">
-          <img className="project-image" src={require("./images/business.jpg")} alt="temp img"/>
-          <p className='project-desc'>A business tracker that keeps track of sales and expenses. Created using React, and uses Firebase to store all of the data. </p>
+          <img className="project-image" src={require("./images/business.jpg")} alt="business tracker"/>
+          <p className='project-desc'>A business tracker that keeps track of sales and expenses. This project was created using React, and uses Firebase to store all of the data. </p>
           <div className='links-div'>
             <a href="https://natehamshar.github.io/business-tracker/" target="_blank" rel="noreferrer" className="live-project">Live Project</a>
             <a href="https://github.com/nateHamshar/business-tracker/" target="_blank" rel="noreferrer" className="project-code">Source Code</a>
           </div>
         </div>
         <div className="project-container">
-          <img className="project-image" src={require("./images/pokedex.JPG")} alt="temp img"/>
+          <img className="project-image" src={require("./images/pokedex.JPG")} alt="pokedex"/>
           <p className='project-desc'>This Pokedex allows a user to search for a pokemon and get some info about them. This is a React project, and uses a public API to get the data.</p>
           <div className='links-div'>
             <a href="https://natehamshar.github.io/offbrand-pokedex/" target="_blank" rel="noreferrer" className="live-project">Live Project</a>
@@ -60,11 +60,11 @@ function App() {
           </div>
         </div>
         <div className="project-container">
-          <img className="project-image" src={require("./images/temp-image.webp")} alt="temp img"/>
-          <p className='project-desc'>This will be a small description of the project that will be here. Probably will include a reason for why the project was made.</p>
+          <img className="project-image" src={require("./images/money.JPG")} alt="money counting app"/>
+          <p className='project-desc'>An interactive learning game designed to help practice counting money. A React project that uses states to keep track of user inputs. </p>
           <div className='links-div'>
-            <a href="google.com" target="_blank" rel="noreferrer" className="live-project">Live Project</a>
-            <a href="google.com" target="_blank" rel="noreferrer" className="project-code">Source Code</a>
+            <a href="https://nateHamshar.github.io/interactive-money-counting" target="_blank" rel="noreferrer" className="live-project">Live Project</a>
+            <a href="https://github.com/nateHamshar/interactive-money-counting" target="_blank" rel="noreferrer" className="project-code">Source Code</a>
           </div>
         </div>
       </div>
